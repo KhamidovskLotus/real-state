65bfbd406042465bf22bc4eeb4c7e36897ad388c
KhamidovskLotus <khamidovsk0409@gmail.com>
2024-08-17 00:29:41 +0500

    Update SectionGridHasMap.tsx

--- src/containers/PropertyPage/SectionGridHasMap.tsx
+++ src/containers/PropertyPage/SectionGridHasMap.tsx
@@ -100,7 +100,7 @@ const SectionGridHasMap: FC<SectionGridHasMapProps> = ({ isLoading = false, isOw
 
         {!showFullMapFixed && (
           <div
-            className="flex xl:hidden items-center justify-center fixed bottom-20 left-1/2 transform -translate-x-1/2 px-6 py-2 bg-neutral-900 text-white shadow-2xl rounded-full z-30  space-x-3 text-sm cursor-pointer"
+            className="flex xl:hidden items-center justify-center fixed bottom-10 left-1/2 transform -translate-x-1/2 px-6 py-2 bg-neutral-900 text-white shadow-2xl rounded-full z-30  space-x-3 text-sm cursor-pointer"
             onClick={() => setShowFullMapFixed(true)}
           >
             <i className="text-lg las la-map"></i>
